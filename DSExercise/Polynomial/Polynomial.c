e63b62913f86bce18adfc3ad0596b431b5cf12e5
muchengzju <muchengzju@qq.com>
2024-04-04 12:47:28 +0800

    Refactor polynomial operations

--- DSExercise/Polynomial/Polynomial.c
+++ DSExercise/Polynomial/Polynomial.c
@@ -26,58 +26,40 @@ void addPoly(pPoly poly1, pPoly poly2, pPoly result) {
     pPoly p = poly1->next;
     pPoly q = poly2->next;
     pPoly r = result;
-    while (p->next != NULL && q->next != NULL) {
-        if (p->exp < q->exp) {
-            addTerm(r, q->coef, q->exp);
-            q = q->next;
-        } else if (p->exp > q->exp) {
-            addTerm(r, p->coef, p->exp);
-            p = p->next;
-        } else {
-            addTerm(r, p->coef + q->coef, p->exp);
-            p = p->next;
-            q = q->next;
-        }
-    }
-    while (p != NULL) {
-        addTerm(r, p->coef, p->exp);
-        p = p->next;
-    }
+
+    // Copy poly2 to result
     while (q != NULL) {
         addTerm(r, q->coef, q->exp);
         q = q->next;
     }
 
+    // Add poly1 to result
+    while (p != NULL) {
+        addTerm(r, p->coef, p->exp);
+        p = p->next;
+    }
+
+    result = r;
 }
 
 void subPoly(pPoly poly1, pPoly poly2, pPoly result) {
     pPoly p = poly1->next;
     pPoly q = poly2->next;
     pPoly r = result;
-    while (p != NULL && q != NULL) {
-        if (p->exp < q->exp) {
-            addTerm(r, -q->coef, q->exp);
-            q = q->next;
-        } else if (p->exp > q->exp) {
-            addTerm(r, p->coef, p->exp);
-            p = p->next;
-        } else {
-            addTerm(r, p->coef - q->coef, p->exp);
-            p = p->next;
-            q = q->next;
-        }
+
+    // Copy negative poly2 to result
+    while (q != NULL) {
+        addTerm(r, - q->coef, q->exp);
+        q = q->next;
     }
+
+    // Add poly1 to result
     while (p != NULL) {
         addTerm(r, p->coef, p->exp);
         p = p->next;
     }
-    while (q != NULL) {
-        addTerm(r, -q->coef, q->exp);
-        q = q->next;
-    }
 
     result = r;
-
 }
 
 void mulPoly(pPoly poly1, pPoly poly2, pPoly result) {
@@ -92,17 +74,19 @@ void mulPoly(pPoly poly1, pPoly poly2, pPoly result) {
 
         q = poly2->next;
         while (q != NULL) {
-            r = result;
+            r = result; // Reset r to the head of result
+
             tempCoef = p->coef * q->coef;
             tempExp = p->exp + q->exp;
 
-            addTerm(r, tempCoef, tempExp);
+            addTerm(r, tempCoef, tempExp); // Add the result term to result polynomial
         
             q = q->next;
         }
 
         p = p->next;
     }
+
     result = r;
 }
 
@@ -141,6 +125,11 @@ void printPoly(pPoly poly) {
 
 void addTerm(pPoly poly, double coef, int exp) {
     pPoly p = poly;
+
+    // 0
+    if (coef == 0) {
+        return;
+    }
 
     // To find the term before the term to be inserted. 
     while (p->next != NULL && p->next->exp >= exp) {
