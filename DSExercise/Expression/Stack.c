c58881599178418d16228fb7efcd8a2b933c3556
muchengzju <muchengzju@qq.com>
2024-04-24 21:18:11 +0800

    Add stack.h header file and implement infix to postfix conversion for arithmetic expressions

---
+++ DSExercise/Expression/Stack.c
@@ -0,0 +1,88 @@
+/**
+ * @file Stack.c
+ * @author MuChengZJU (MuChengZJU@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-24
+ * 
+ * 
+ */
+#include "Stack.h"
+
+int cStackInit(charStack *cStack) {
+    cStack->base = (char *)malloc(STACK_INIT_SIZE * sizeof(char));
+    if (!cStack->base) {
+        return -1;
+    }
+    cStack->top = cStack->base;
+    cStack->stackSize = STACK_INIT_SIZE;
+    return 0;
+}
+
+int cStackPush(charStack *cStack, char c){
+    if (cStack->top - cStack->base >= cStack->stackSize) {
+        cStack->base = (char *)realloc(cStack->base, (cStack->stackSize + STACK_INIT_SIZE) * sizeof(char));
+        if (!cStack->base) {
+            return -1;
+        }
+        cStack->top = cStack->base + cStack->stackSize;
+        cStack->stackSize += STACK_INIT_SIZE;
+    }
+    *cStack->top++ = c;
+    return 0;
+}
+
+int cStackPop(charStack *cStack, char *c) {
+    if (cStack->top == cStack->base) {
+        return -1;
+    }
+    *c = *--cStack->top;
+    return 0;
+}
+
+int cStackDestroy(charStack *cStack) {
+    free(cStack->base);
+    cStack->base = NULL;
+    cStack->top = NULL;
+    cStack->stackSize = 0;
+    return 0;
+}
+
+int dStackInit(doubleStack *dStack) {
+    dStack->base = (double *)malloc(STACK_INIT_SIZE * sizeof(double));
+    if (!dStack->base) {
+        return -1;
+    }
+    dStack->top = dStack->base;
+    dStack->stackSize = STACK_INIT_SIZE;
+    return 0;
+}
+
+int dStackPush(doubleStack *dStack, double d) {
+    if (dStack->top - dStack->base >= dStack->stackSize) {
+        dStack->base = (double *)realloc(dStack->base, (dStack->stackSize + STACK_INIT_SIZE) * sizeof(double));
+        if (!dStack->base) {
+            return -1;
+        }
+        dStack->top = dStack->base + dStack->stackSize;
+        dStack->stackSize += STACK_INIT_SIZE;
+    }
+    *dStack->top++ = d;
+    return 0;
+}
+
+int dStackPop(doubleStack *dStack, double *d) {
+    if (dStack->top == dStack->base) {
+        return -1;
+    }
+    *d = *--dStack->top;
+    return 0;
+}
+
+int dStackDestroy(doubleStack *dStack) {
+    free(dStack->base);
+    dStack->base = NULL;
+    dStack->top = NULL;
+    dStack->stackSize = 0;
+    return 0;
+}
---
+++ DSExercise/Expression/Stack.h
@@ -0,0 +1,106 @@
+/**
+ * @file Stack.h
+ * @author MuChengZJU (MuChengZJU@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-24
+ * 
+ * 
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_INIT_SIZE 100
+
+/**
+ * @brief (For Operators) Infix to Postfix, Postfix expression calculation stack in Expression.c
+ * 
+ */
+typedef struct {
+    char *base;
+    char *top;
+    int stackSize;
+}charStack;
+
+/**
+ * @brief (For Numbers) Postfix expression calculation stack in Expression.c
+ * 
+ */
+typedef struct {
+    double *base;
+    double *top;
+    int stackSize;
+}doubleStack;
+
+/**
+ * @brief Init charStack
+ * 
+ * @param cStack 
+ * @return int 
+ */
+int cStackInit(charStack *cStack);
+
+/**
+ * @brief Push char to charStack
+ * 
+ * @param cStack 
+ * @param c 
+ * @return int 
+ */
+int cStackPush(charStack *cStack, char c);
+
+/**
+ * @brief Pop char from charStack
+ * 
+ * @param cStack 
+ * @param c 
+ * @return int 
+ */
+int cStackPop(charStack *cStack, char *c);
+
+/**
+ * @brief Destroy charStack
+ * 
+ * @param cStack 
+ * @return int 
+ */
+int cStackDestroy(charStack *cStack);
+
+
+/**
+ * @brief Init doubleStack
+ * 
+ * @param dStack 
+ * @return int 
+ */
+int dStackInit(doubleStack *dStack);
+
+/**
+ * @brief Push double to doubleStack
+ * 
+ * @param dStack 
+ * @param d 
+ * @return int 
+ */
+int dStackPush(doubleStack *dStack, double d);
+
+/**
+ * @brief Pop double from doubleStack
+ * 
+ * @param dStack 
+ * @param d 
+ * @return int 
+ */
+int dStackPop(doubleStack *dStack, double *d);
+
+/**
+ * @brief Destroy doubleStack
+ * 
+ * @param dStack 
+ * @return int 
+ */
+int dStackDestroy(doubleStack *dStack);
+
+
+#endif // STACK_H
