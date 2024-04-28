b2a6ea1b7df66406167dd0d210b399a9382c7a91
muchengzju <muchengzju@qq.com>
2024-04-28 23:42:39 +0800

    KMP跑起来了

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -4,8 +4,8 @@
  * @brief https://jnnls4etqn.feishu.cn/wiki/ZB1awtYcdiqQdFkQw35c8BWYnhg
  * @version 0.1
  * @date 2024-04-27
- * 
- * 
+ *
+ *
  */
 
 #include <stdio.h>
@@ -14,10 +14,11 @@
 
 /**
  * @brief String data structure definition
- * 
+ *
  */
-typedef struct {
-    char* str;
+typedef struct
+{
+    char *str;
     int length;
 } String;
 
@@ -25,41 +26,49 @@ typedef struct {
  * @brief Node for string
  * @brief header node: chdata is "h", succ points to the first node
  */
-typedef struct Node {
+typedef struct Node
+{
     char chdata;
-    struct Node* succ;
-    struct Node* next;
+    struct Node *succ;
+    struct Node *next;
 } Node;
 
 /**
- * @brief Replace substring T in S with V. 
+ * @brief Replace substring T in S with V.
  * For example, Replace("Hello, world!", "world", "C language") will return "Hello, C language!"
- * 
+ *
  * @param S Source string to be replaced
  * @param T Substring to be replaced
  * @param V New substring to replace with
  */
-void Replace(String* S, String T, String V) {
+void Replace(String *S, String T, String V)
+{
     int i, j, k;
     int pos = 0;
     int new_length = S->length;
 
     // Calculate and allocate new length
-    for (i = 0; i <= S->length - T.length; i++) {
-        if (strncmp(S->str + i, T.str, T.length) == 0) {
+    for (i = 0; i <= S->length - T.length; i++)
+    {
+        if (strncmp(S->str + i, T.str, T.length) == 0)
+        {
             new_length = new_length - T.length + V.length;
         }
     }
-    char* new_str = (char*)malloc(new_length * sizeof(char));
+    char *new_str = (char *)malloc(new_length * sizeof(char));
 
     // Replace substring
-    for (i = 0, j = 0; i < S->length; i++) {
-        if (strncmp(S->str + i, T.str, T.length) == 0) { // Find T
+    for (i = 0, j = 0; i < S->length; i++)
+    {
+        if (strncmp(S->str + i, T.str, T.length) == 0)
+        { // Find T
             // Append V to new_str
             strcpy(new_str + j, V.str);
             j += V.length;
             i += T.length - 1;
-        } else {
+        }
+        else
+        {
             // Copy source string to new_str
             new_str[j++] = S->str[i];
         }
@@ -73,25 +82,32 @@ void Replace(String* S, String T, String V) {
 
 /**
  * @brief Get next pointer for KMP algorithm
- * 
+ *
  * @param pattern Pointer to head node of pattern string
  */
-void getNext(Node *pattern) {
-    Node *currNode = pattern->succ; // Jump off the head node TODO: from 1st or 2nd node
-    Node *succNode = currNode->succ;
-    currNode->next = currNode; // First node's next is itself
-    Node* nextPtr = currNode->next;
-    while (currNode->chdata != '\n') {
-        if (currNode->chdata == nextPtr->chdata || nextPtr == pattern) {
-            currNode->succ->next = nextPtr->succ;
-            currNode = succNode; // Move to next node
-        } else {
-            nextPtr = nextPtr->next; // Recursive
+void getNext(Node *pattern)
+{
+    Node *p = pattern->succ; // p points to the first node
+    Node *q = pattern;       // q points to the head node
+    p->next = q;
+
+    while (p->succ != NULL)
+    {
+        if (q == pattern || p->chdata == q->chdata)
+        {
+            p = p->succ;
+            q = q->succ;
+            p->next = q;
+        }
+        else
+        {
+            q = q->next;
         }
     }
 }
 
-int main() {
+int main()
+{
     // 4.17 Replace(&S, T, V).
     String S, T, V;
     S.str = "Hello, world! Good morning, world!";
@@ -102,7 +118,7 @@ int main() {
     V.length = strlen(V.str);
 
     Replace(&S, T, V);
-    
+
     printf("New String: %s\n", S.str);
 
     // free(S.str); // No need to free S.str, because it is not malloced in this demo
@@ -119,20 +135,36 @@ int main() {
     // input pattern string
     printf("Input pattern string: ");
     char ch;
-    while ((ch = getchar()) != '\n') {
+    while ((ch = getchar()) != '\n')
+    {
         p->chdata = ch;
         p->succ = (Node *)malloc(sizeof(Node));
         p = p->succ;
         p->next = NULL;
     }
     // end of pattern string
-    p->chdata = '\n';
+    p->chdata = '\0';
     p->succ = NULL;
 
     // getNext
     getNext(pattern);
 
-    free(pattern);
+    // Print the pattern string and next pointers
+    Node *temp = pattern->succ; // Skip the head node
+    while (temp != NULL && temp->succ != NULL)
+    {
+        printf("Node: %c, Next: %p, *Next: %c\n", temp->chdata, temp->next, temp->next->chdata);
+        temp = temp->succ;
+    }
+
+    // Free the linked list
+    p = pattern;
+    while (p != NULL)
+    {
+        Node *temp = p;
+        p = p->succ;
+        free(temp);
+    }
 
     return 0;
 }
